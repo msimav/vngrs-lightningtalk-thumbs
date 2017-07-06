6607b166f3a388412d6a0b76f0d5fe06c84233be
Mustafa Simav <mustafa@vngrs.com>
2017-07-06 15:28:53 +0300

    Produce jpeg instead of png

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-convert background.png -gravity South -pointsize 96 -font 'KelsonSans-Regular' -fill white -annotate +0+300 "$1" -pointsize 64 -annotate +0+220 "$2" out/thumbnail.png
+convert background.png -gravity South -pointsize 96 -font 'KelsonSans-Regular' -fill white -annotate +0+300 "$1" -pointsize 64 -annotate +0+220 "$2" out/thumbnail.jpg
